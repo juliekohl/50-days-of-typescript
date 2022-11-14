d26f71184f65e5750449984b387d9820685300c4
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-11-14 16:21:37 -0300

    "first commit"

---
+++ day-1-printing-start/src/index.ts
@@ -0,0 +1,42 @@
+// Write your code below this line
+// let message:string = "Hello Julie!";
+// console.log(1, message);
+
+// Printing String
+// console.log("Day 1 - TypeScript console.log Function");
+// console.log("The function is declared like this:");
+// console.log("console.log('what to print')");
+
+// printing with concatenation
+// console.log("Day 1 - String Manipulation");
+// console.log('String Concatenation is done with the "+" sign.');
+// console.log('e.g. print("Hello " + "world")');
+// console.log("New lines can be created with a backslash and n.\nlike this");
+
+// Prints the number of char in my name use prompt
+// let lengthName: number = "Juliana".length;
+// console.log(lengthName);
+
+// variables
+// console.log("What is your name? ");
+// let myName:string = "Juliana";
+// console.log(myName);
+//
+// let yourName:string = "Cesar";
+// console.log(yourName);
+//
+// let lengthName: number = myName.length;
+// console.log(lengthName);
+
+// let a:any = 5;
+// let b:any = 100;
+//
+// console.log("a: " + b);
+// console.log("b: " + a);
+
+// Project Band Name Generator
+// console.log("Welcome to the Band Name Generator.")
+// let city_name:string = "Curitiba"
+// let pet_name:string = "Baluu"
+//
+// console.log("Your band name could be " + city_name + " " + pet_name)
