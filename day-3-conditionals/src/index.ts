0988d58b41c0efacfe0fac23ba171c2b7232cdce
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-11-23 10:36:19 -0300

    If, else if, else.

--- day-3-conditionals/src/index.ts
+++ day-3-conditionals/src/index.ts
@@ -18,3 +18,21 @@
 //     console.log("This is an odd number.");
 // }
 
+// Nested if, else if, else
+// console.log("Welcome to the rollercoaster!");
+// let height: number = 121;
+//
+// if (height >= 120) {
+//     console.log("You can ride the rollercoaster!");
+//     let age: number = 10;
+//
+//     if (age < 12) {
+//         console.log("Please pay $5.");
+//     } else if (age <= 18) {
+//         console.log("Please pay $7.");
+//     } else {
+//         console.log("Please pay $12.");
+//     }
+// } else {
+//     console.log("Sorry, you have to grow taller before you can ride.");
+// }
