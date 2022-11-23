cb5da5748690732cf795f42fe6f4262c0f94dcc1
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-11-23 10:34:49 -0300

    Day 3 conditionals

---
+++ day-3-conditionals/src/index.ts
@@ -0,0 +1,11 @@
+// Conditional
+
+// console.log("Welcome to the rollercoaster!");
+// let height: number = 157
+//
+// if (height >= 120) {
+//     console.log("You can ride the rollercoaster!");
+// } else {
+//     console.log("Sorry, you have to grow taller before you can ride.");
+// }
+
