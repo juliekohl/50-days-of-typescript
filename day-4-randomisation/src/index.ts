1793271a02b0827e99c0779e3b4c5541bccf3e5c
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-11-24 10:21:09 -0300

    Exercise Banker Roulette.

--- day-4-randomisation/src/index.ts
+++ day-4-randomisation/src/index.ts
@@ -12,23 +12,33 @@
 // }
 
 // Offset and appending items to lists
-let statesOfAmerica: string[] = [
-    "Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland",
-    "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont",
-    "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine",
-    "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota",
-    "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota",
-    "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"
-];
-console.log(statesOfAmerica);
-console.log(statesOfAmerica[0]); // Delaware
+// let statesOfAmerica: string[] = [
+//     "Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland",
+//     "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont",
+//     "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine",
+//     "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota",
+//     "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota",
+//     "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"
+// ];
+// console.log(statesOfAmerica);
+// console.log(statesOfAmerica[0]); // Delaware
+//
+// statesOfAmerica.splice(3, 1, "Georgya"); // replace to Georgia for Georgya
+// console.log(1, statesOfAmerica);
+//
+// let dirtyDozen: string[] = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"];
+// let concat: string[] = statesOfAmerica.concat(dirtyDozen);
+// console.log(concat);
+//
+// console.log("push", statesOfAmerica.push(...dirtyDozen)); // 62
+// console.log("new", statesOfAmerica); // add new array to the end
 
-statesOfAmerica.splice(3, 1, "Georgya"); // replace to Georgia for Georgya
-console.log(1, statesOfAmerica);
+// Exercise Banker Roulette
+let friendsNames: string = "Juliana, Angie, Kelly, Nathi, Gio";
+let splitNames: string[] = friendsNames.split(", ");
+let randomChoice: number = Math.floor(Math.random() * splitNames.length);
+// console.log("randomChoice to pay", randomChoice, splitNames[randomChoice]); // 4 Gio
+console.log(splitNames[randomChoice] + " is going to buy the meal today!");
 
-let dirtyDozen: string[] = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"];
-let concat: string[] = statesOfAmerica.concat(dirtyDozen);
-console.log(concat);
-
-console.log("push", statesOfAmerica.push(...dirtyDozen)); // 62
-console.log("new", statesOfAmerica); // add new array to the end
+// let personPay: string = splitNames[randomChoice];
+// console.log(personPay + " is going to buy the meal today!"); // pick out random person
