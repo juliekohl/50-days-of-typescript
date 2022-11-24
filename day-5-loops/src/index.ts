cd388961debce91706fd40db3c07c2245fc89a7c
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-11-24 15:16:14 -0300

    Loops.

---
+++ day-5-loops/src/index.ts
@@ -0,0 +1,13 @@
+// For Loop with lists
+let fruits: string[] = ["Apple", "Peach", "Pear"];
+// console.log(fruits);
+
+// for (let fruit in fruits) {
+//   console.log("fruit", fruits[fruit]);
+//   console.log(fruits[fruit] + " Pie");
+//   console.log(fruits);
+// }
+
+// fruits.map(fruit => console.log(fruit));
+
+fruits.map(fruit => console.log(fruit + " Pie"));
