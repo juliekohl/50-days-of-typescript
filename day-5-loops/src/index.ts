c6034f8c30897fc0806a4067a4523e44ad30a865
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-11-25 10:22:19 -0300

    High Score.

--- day-5-loops/src/index.ts
+++ day-5-loops/src/index.ts
@@ -1,5 +1,5 @@
 // For Loop with lists
-let fruits: string[] = ["Apple", "Peach", "Pear"];
+// let fruits: string[] = ["Apple", "Peach", "Pear"];
 // console.log(fruits);
 
 // for (let fruit in fruits) {
@@ -10,4 +10,25 @@ let fruits: string[] = ["Apple", "Peach", "Pear"];
 
 // fruits.map(fruit => console.log(fruit));
 
-fruits.map(fruit => console.log(fruit + " Pie"));
+// fruits.map(fruit => console.log(fruit + " Pie"));
+
+// High Score
+let studentHeights: string[] = "156 178 165 171 187".split(" ");
+studentHeights.map(studentScores => console.log(2, Number(studentScores)));
+
+let totalHeight: number = 0;
+for (let height in studentHeights) {
+    console.log("height", Number(studentHeights[height]));
+    totalHeight += Number(studentHeights[height]);
+    console.log(`total height = ${totalHeight}`);
+}
+
+let NumberOfStudents: number = 0;
+for (let student in studentHeights) {
+    console.log("Number of students", Number(studentHeights[student]));
+    NumberOfStudents += 1;
+    console.log(`Number of students = ${NumberOfStudents}`);
+}
+
+let average_height: number = Math.round(totalHeight / NumberOfStudents);
+console.log("average_height", average_height);
