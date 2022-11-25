5518f9e5759b58c7ff2b0a36ef092246376171be
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-11-25 11:44:42 -0300

    range list of numbers.

--- day-5-loops/src/index.ts
+++ day-5-loops/src/index.ts
@@ -34,13 +34,75 @@
 // console.log("average_height", average_height);
 
 // High Score
-let studentScores: number[] = [78, 65, 89, 86, 55, 100, 64, 89, 90];
-studentScores.map(score => {
-    let heighestScore: number = 0;
-    for (let score in studentScores) {
-        if (studentScores[score] > heighestScore) {
-            heighestScore = studentScores[score];
-        }
-    };
-    console.log(`The highest score in the class is: ${heighestScore}`);
-});
+// let studentScores: number[] = [78, 65, 89, 86, 55, 100, 64, 89, 90];
+// studentScores.map(score => {
+//     let heighestScore: number = 0;
+//     for (let score in studentScores) {
+//         if (studentScores[score] > heighestScore) {
+//             heighestScore = studentScores[score];
+//         }
+//     };
+//     console.log(`The highest score in the class is: ${heighestScore}`);
+// });
+
+// Random
+// const range: number[] = [...Array(10).keys()];
+// console.log(range); // [0,1,2,3,4,5,6,7,8,9]
+
+// const start: number = 0;
+// const end: number = 9;
+// const step: number = 1;
+// const arrayLength: number = Math.floor(((end - start) / step)) + 1;
+// const range: number[] = [...Array(arrayLength).keys()].map(x => (x * step) + start);
+// console.log("range", range); // [0,1,2,3,4,5,6,7,8,9]
+
+// const start: number = 1;
+// const end: number = 11;
+// const range: number[] = [...Array(end - start + 1).keys()].map(x => x + start);
+// console.log("range", range); // [1,2,3,4,5,6,7,8,9,10,11]
+
+// const start: number = 1;
+// const end: number = 11;
+// const step: number = 3;
+// const arrayLength: number = Math.floor(((end - start) / step)) + 1;
+// const range: number[] = [...Array(arrayLength).keys()].map(x => (x * step) + start);
+// console.log("range", range); // [1,4,7,10]
+
+// total
+// const start: number = 1;
+// const end: number = 100;
+// const range: number[] = [...Array(end - start + 1).keys()].map(x => x + start);
+//
+// let sum: number = 0;
+// for (let number in range) {
+//     console.log("number", range[number]);
+//     sum += range[number]
+// }
+// console.log("sum", sum); // 5050
+
+// Add even numbers
+// const start: number = 2;
+// const end: number = 101;
+// const step: number = 2;
+// const arrayLength: number = Math.floor(((end - start) / step)) + 1;
+// const range: number[] = [...Array(arrayLength).keys()].map(x => (x * step) + start);
+// let evenSum: number = 0;
+// for (let number in range) {
+//     console.log("number", range[number]);
+//     evenSum += range[number];
+// }
+// console.log("evenSum", evenSum); // 2550
+
+// OR
+// const start: number = 2;
+// const end: number = 101;
+// const range: number[] = [...Array(end - start + 1).keys()].map(x => x + start);
+//
+// let evenSum: number = 0;
+// for (let number in range) {
+//     console.log("number", range[number]);
+//     if (range[number] % 2 === 0) {
+//         evenSum += range[number]
+//     }
+// }
+// console.log("evenSum", evenSum); // 2550
