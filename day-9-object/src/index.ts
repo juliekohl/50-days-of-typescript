d780d7110624161a0fdd6d16ee0a21f73b4254d5
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-12-07 17:19:18 -0300

    Day 9 - Nesting objects and array.

--- day-9-object/src/index.ts
+++ day-9-object/src/index.ts
@@ -37,31 +37,69 @@ const programmingDictionary: object = {
 // }
 
 // Exercise Grading Program
-let studentScores: object = {
-    Harry: 81,
-    Ron: 78,
-    Hermione: 99,
-    Draco: 74,
-    Neville: 67,
-};
+// let studentScores: object = {
+//     Harry: 81,
+//     Ron: 78,
+//     Hermione: 99,
+//     Draco: 74,
+//     Neville: 67,
+// };
+//
+// let studentGrades: object = {};
+//
+// for (let student in studentScores) {
+//     let score: number = studentScores[student];
+//
+//     if (score > 90){
+//         studentGrades[student] = "Outstanding";
+//         console.log(studentGrades[student] = "Outstanding");
+//     } else if (score > 80) {
+//         studentGrades[student] = "Exceeds Expectations";
+//         console.log(studentGrades[student] = "Exceeds Expectations");
+//     } else if (score > 70) {
+//         studentGrades[student] = "Acceptable";
+//         console.log(studentGrades[student] = "Acceptable");
+//     } else {
+//         studentGrades[student] = "Fail";
+//         console.log(studentGrades[student] = "Fail");
+//     }
+// }
+// console.log(studentGrades);
 
-let studentGrades: object = {};
+// Nesting Lists and Dictionaries
+// Nesting
+// let capitals: object = {
+//     france: "Paris",
+//     germany: "Berlin",
+// };
+// console.log(capitals["france"]) // Paris return value
 
-for (let student in studentScores) {
-    let score: number = studentScores[student];
+// Nesting a List in a Dictionary
+// let travelLog: object = {
+//     france: ["Paris", "Lille", "Dijon"],
+//     germany: ["Berlin", "Hamburg", "Stuttgart"],
+// };
+// console.log(travelLog["germany"]) // germany return value ["Berlin","Hamburg","Stuttgart"]
 
-    if (score > 90){
-        studentGrades[student] = "Outstanding";
-        console.log(studentGrades[student] = "Outstanding");
-    } else if (score > 80) {
-        studentGrades[student] = "Exceeds Expectations";
-        console.log(studentGrades[student] = "Exceeds Expectations");
-    } else if (score > 70) {
-        studentGrades[student] = "Acceptable";
-        console.log(studentGrades[student] = "Acceptable");
-    } else {
-        studentGrades[student] = "Fail";
-        console.log(studentGrades[student] = "Fail");
-    }
-}
-console.log(studentGrades);
+// Nesting Dictionary in a Dictionary
+// let travelLog: object = {
+//     france: {"citiesVisited": ["Paris", "Lille", "Dijon"], "totalVisits": 12},
+//     germany: {"citiesVisited": ["Berlin", "Hamburg", "Stuttgart"], "totalVisits": 5},
+// };
+// console.log(travelLog["france"]) // france return value
+
+// Nesting Dictionaries in Lists
+let travelLog: [{}, {}] = [
+    {
+        country: "France",
+        citiesVisited: ["Paris", "Lille", "Dijon"],
+        totalVisits: 12,
+    },
+    {
+        country: "Germany",
+        citiesVisited: ["Berlin", "Hamburg", "Stuttgart"],
+        totalVisits: 5,
+    },
+];
+console.log(1, travelLog[0]) // France return value
+console.log(2, travelLog[0]["citiesVisited"]) // citiesVisited return value ["Paris","Lille","Dijon"]
