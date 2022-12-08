53fc9034c9d7579680e796a1853c4d0153e4dc39
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-12-08 14:33:13 -0300

    Quiz exercises.

--- day-10-functions-outputs/src/index.ts
+++ day-10-functions-outputs/src/index.ts
@@ -23,34 +23,82 @@
 // formatName(" ", " Kohl");
 
 // Exercise Days in Month
-let year: number = 1992;
-let month: number = 2;
-let days: string | number = daysInMonth(year, month);
+// let year: number = 1992;
+// let month: number = 2;
+// let days: string | number = daysInMonth(year, month);
+//
+// function isLeap(year): boolean {
+//     if (year % 4 == 0) {
+//         if (year % 100 == 0) {
+//             return year % 400 == 0;
+//         } else {
+//             return true;
+//         }
+//     } else {
+//         return false;
+//     }
+// }
+//
+// function daysInMonth(year, month) {
+//     let monthDays: number[] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+//
+//     if (month > 12 || month < 1) {
+//         return "Invalid month entered";
+//     }
+//
+//     if (month === 2 && isLeap(year)) {
+//         return 29;
+//     }
+//
+//     return monthDays[month - 1];
+// }
+//
+// console.log(days);
 
-function isLeap(year): boolean {
-    if (year % 4 == 0) {
-        if (year % 100 == 0) {
-            return year % 400 == 0;
-        } else {
-            return true;
-        }
-    } else {
-        return false;
-    }
-}
+// Quiz
+// 1
+// let n1: number = 0;
+// let n2: number = 0;
+//
+// function add(n1, n2): number {
+//     return n1 + n2;
+// }
+//
+// function subtract(n1, n2): number {
+//     return n1 - n2;
+// }
+//
+// function multiply(n1, n2): number {
+//     return n1 * n2;
+// }
+//
+// function divide(n1, n2): number {
+//     return n1 / n2;
+// }
+//
+// add(2, multiply(5, divide(8, 4))); //12
+// add(2, subtract(2, multiply(5, divide(8, 4)))); //-6
 
-function daysInMonth(year, month) {
-    let monthDays: number[] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+// 2
+// function outerFunction(a: number, b: number): number {
+//     function innerFunction(c: number, d: number): number {
+//         return c + d;
+//     }
+//     return innerFunction(a, b);
+// }
+//
+// let result = outerFunction(5, 10);
+// console.log("result", result); //15
 
-    if (month > 12 || month < 1) {
-        return "Invalid month entered";
-    }
-
-    if (month === 2 && isLeap(year)) {
-        return 29;
-    }
-
-    return monthDays[month - 1];
-}
-
-console.log(days);
+// 3
+// function myFunction(a: number): string {
+//     if (a < 40) {
+//         return 'Terrible';
+//     } else if (a < 80) {
+//         return 'Pass';
+//     } else {
+//         return '"Great';
+//     }
+// }
+//
+// myFunction(25); // Terrible
