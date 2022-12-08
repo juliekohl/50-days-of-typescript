b3ddabf84cf242d9cf2f987f1cbb390f74a92ead
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-12-08 13:19:37 -0300

    Multiple return values.

--- day-10-functions-outputs/src/index.ts
+++ day-10-functions-outputs/src/index.ts
@@ -1,8 +1,23 @@
-// Functions Outputs
+// Function Outputs
+// function formatName(fName: string, lName: string): string {
+//     let formattedFName: string = fName.charAt(0).toUpperCase() + fName.slice(1).toLowerCase();
+//     let formattedLName: string = lName.charAt(0).toUpperCase() + lName.slice(1).toLowerCase();
+//     return `${formattedFName} ${formattedLName}`;
+// }
+//
+// formatName("JuLiE", "KoHl");
+
+// Multiple return values
 function formatName(fName: string, lName: string): string {
+    if (fName === " " || lName === " ") {
+        console.log(1, "You didn't provide valid inputs.");
+        return "You didn't provide valid inputs.";
+    }
+
     let formattedFName: string = fName.charAt(0).toUpperCase() + fName.slice(1).toLowerCase();
     let formattedLName: string = lName.charAt(0).toUpperCase() + lName.slice(1).toLowerCase();
-    return formattedFName + " " + formattedLName;
+
+    return `Result ${formattedFName} ${formattedLName}`;
 }
 
-formatName("JuLiE", "KoHl");
+formatName(" ", " Kohl");
