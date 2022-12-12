bc47926b1a12e53696ccece54f03c0ae830f5031
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-12-12 17:04:17 -0300

    Project - Number Guessing Game.

--- day-12-local-global-scope/src/index.ts
+++ day-12-local-global-scope/src/index.ts
@@ -50,19 +50,73 @@
 // createEnemy();
 
 // Modify a Global Variable
-let enemies: number = 1;
-
-function increaseEnemies(): number {
-    console.log(`enemies inside function: ${enemies}`); // 1
-    return enemies ++;
-}
-
-console.log(`enemies outside function: ${enemies}`); // 2
-
-increaseEnemies();
-console.log(`enemies outside function: ${enemies}`); // 2
+// let enemies: number = 1;
+//
+// function increaseEnemies(): number {
+//     console.log(`enemies inside function: ${enemies}`); // 1
+//     return enemies ++;
+// }
+//
+// console.log(`enemies outside function: ${enemies}`); // 2
+//
+// increaseEnemies();
+// console.log(`enemies outside function: ${enemies}`); // 2
 
 // Constants & Global Scope
-const pi: number = 3.14159;
-let url: string = "https://www.google.com";
-const twitter: string = "@JCochenski";
+// const pi: number = 3.14159;
+// let url: string = "https://www.google.com";
+// const twitter: string = "@JCochenski";
+
+// Project - Number Guessing Game
+const easyLevelTurns: number = 10;
+const hardLeverTurns: number = 5;
+
+function checkAnswer(guess, answer, turns): number {
+    if (guess > answer) {
+        console.log("Too high.");
+        return turns --;
+    } else if (guess < answer) {
+        console.log("Too low.");
+        return turns --;
+    } else {
+        console.log(`You got it! The answer was ${answer}.`);
+        return answer;
+    }
+}
+
+function setDifficulty(): number {
+    let level: string = "hard";
+
+    if (level === "easy") {
+        console.log("easy", easyLevelTurns);
+        return easyLevelTurns;
+    } else {
+        console.log("hard", hardLeverTurns);
+        return hardLeverTurns;
+    }
+}
+
+function game(): void {
+    console.log("Welcome to the Number Guessing Game!");
+    console.log("I'm thinking of a number between 1 and 100.");
+
+    let answer: number =  Math.floor(Math.random() * 100);
+    console.log("answer", answer);
+
+    let turns: number = setDifficulty();
+    let guess: number = 0;
+
+    if (guess !== answer) {
+        console.log(`You have ${turns} attempts remaining to guess the number.`);
+        guess = 72;
+
+        turns = checkAnswer(guess, answer, turns);
+        if (turns === 0) {
+            console.log("You've run out of guesses, you lose.");
+        }
+    } else if (guess !== answer) {
+        console.log("Guess again.");
+    }
+}
+
+game();
