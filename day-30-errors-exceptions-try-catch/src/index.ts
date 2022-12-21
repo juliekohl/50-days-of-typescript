470cf55f9047fd1c160b036c4c2ec2ad435a71b8
Juliana Cochenski <juliana.cochenski@gmail.com>
2022-12-21 10:08:12 -0300

    Try catch error.

--- day-30-errors-exceptions-try-catch/src/index.ts
+++ day-30-errors-exceptions-try-catch/src/index.ts
@@ -1,18 +1,33 @@
 // Try catch error
 // BMI Example
-function bmi(height, weight) {
-    try {
-        if (height > 3) {
-            throw "Human Height should not be over 3 meters."
-        }
+// function bmi(height: number, weight: number): void {
+//     try {
+//         if (height > 3) {
+//             throw "Human Height should not be over 3 meters."
+//         }
+//
+//         let result: number = weight / height ** 2;
+//         console.log(result);
+//     } catch (e) {
+//         console.log("error:", e);
+//     }
+// }
+//
+// bmi( 50, 600); // "Human Height should not be over 3 meters."
 
-        let result: number = weight / height ** 2;
-        console.log(result);
+// Make pai error
+let fruits: string[] = ["Apple", "Pear", "Orange"];
+
+function makePie(fruit: number): void {
+    try {
+        if (fruits[fruit] === undefined) {
+            throw "Fruit pie.";
+        }
+        console.log(`${fruits[fruit]} pie.`);
+
     } catch (e) {
         console.log("error:", e);
     }
 }
 
-bmi( 50, 600); // "Human Height should not be over 3 meters."
-
-
+console.log(makePie(4));
